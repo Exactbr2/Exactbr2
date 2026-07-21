9e278af50cdbd154cf12c69a8377aa682212d8e3
Exactbr2 <evanevan248@outlook.com>
2026-07-21 00:13:08 -0500

    Add files via upload

---
+++ web.txt/exactbrush.js
@@ -0,0 +1,8 @@
+// Select all menu items
+const fileInput = document.getElementById('myFile');
+const triggerBtn0 = document.querySelector('.labelcent2');
+
+// Open file menu when the image is clicked
+triggerBtn0.addEventListener('click', () => {
+fileInput.click();
+});
